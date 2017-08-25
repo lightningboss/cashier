b67f5701f97a1fa9f7ba927cc44e278520c11337
Marc Nitzsche <marc.nitzsche@live.de>
2017-08-25 13:10:10 +0200

    Add react-fastclick

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import initReactFastclick from 'react-fastclick';
+
+import registerServiceWorker from './registerServiceWorker';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+
+initReactFastclick();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
